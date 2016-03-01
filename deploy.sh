b9bf47020167e11c7e1a20e008dbc50edbf69e27
Carlos Badenes <cbadenes@gmail.com>
2016-03-01 15:59:54 +0100

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "stopping the server ..."
+/opt/drinventor/learner/bin/catalina.sh stop
+
+echo "removing old applications .."
+rm -rf /opt/drinventor/learner/webapps/api*
+rm -rf /opt/drinventor/learner/webapps/learner*
+
+echo "copying the new ones .."
+cp api/target/api.war /opt/drinventor/learner/webapps
+cp api/target/learner.war /opt/drinventor/learner/webapps
+
+#echo "starting the server .."
+#/opt/drinventor/learner/bin/catalina.sh start

51b5e605ecf3c376085a63765d3e0f27098a7433
David Chaves <dachafra@gmail.com>
2016-06-21 10:17:31 +0200

    Some changes

--- learner/src/test/java/org/epnoi/learner/LearnerTest.java
+++ learner/src/test/java/org/epnoi/learner/LearnerTest.java
@@ -25,9 +25,9 @@ import java.util.*;
 
 /**
  * Created by rgonzalez on 3/12/15.
- * Modified by dachafra on 4/4/16
+ * Modified by dachafra on 4/4/16.
  */
-@Category(IntegrationTest.class)
+//@Category(IntegrationTest.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = LearnerConfig.class)
 @ActiveProfiles("develop")
